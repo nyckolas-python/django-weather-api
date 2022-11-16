f73a8d5e357746ebec625f05d1ee1e298a3b0193
nyckolas <nyckolas.work@gmail.com>
2022-11-16 22:29:26 +0200

    Add celery task view

--- api/views.py
+++ api/views.py
@@ -5,16 +5,23 @@ from rest_framework.response import Response
 
 from api.serializers import WeatherSerializer
 from api.models import Weather
-from api.servises import update_or_create_weather_task
+from api.tasks import update_or_create_weather_task, sample_task
 
 
 # Create your views here.
 class WeathersListView(ListAPIView):
+    """
+        Return list of weather forecast
+    """
     permission_classes = (AllowAny,)
     queryset = Weather.objects.all()
     serializer_class = WeatherSerializer
 
 class TaskStarterView(APIView):
+    """
+        Run weather data scraping task and return task id
+    """
     def get(self, request, format=None):
-        update_or_create_weather_task()
-        return Response('Data was updated...')
+        task = update_or_create_weather_task.delay()
+        
+        return Response(f"The weather data scraping is {task.state}. Task id: {task.id}")
