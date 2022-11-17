e6f442a025f0ee08d7761f354cb7cd28b0418890
nyckolas <nyckolas.work@gmail.com>
2022-11-17 22:59:05 +0200

    Add schedule-task url

--- api/urls.py
+++ api/urls.py
@@ -4,6 +4,7 @@ from api import views
 
 urlpatterns = [
     path('v1/forecast/', views.WeathersListView.as_view()),
-    path('v1/forecast/task/', views.TaskStarterView.as_view()),
+    path('v1/forecast/run-task/', views.TaskStarterView.as_view()),
+    path('v1/forecast/schedule-task/', views.TaskSchedulerView.as_view()),
     
 ]
